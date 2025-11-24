d7272a23f6d072fc51f40661f9f4b60489af1e29
Sean <seanwalkerb+github@pm.me>
2025-11-24 20:20:00 +1000

    Update hyprland.sh

--- hyprland.sh
+++ hyprland.sh
@@ -1,5 +1,8 @@
 sudo dnf copr enable solopasha/hyprland
-sudo dnf install hyprland sddm kitty waybar hyprpolkitagent mako xdg-desktop-portal-hyprland
+sudo dnf install hyprland sddm kitty waybar hyprpolkitagent mako xdg-desktop-portal-hyprland pavucontrol
+sudo systemctl enable sddm.service
+sudo systemctl set-default graphical.target
+systemctl --user enable --now hyprpolkitagent.service
 
 sudo dnf install dnf-plugins-core
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
