10786263a230d8c532d0baeeb4bc02e22c47e20f
Sean <seanwalkerb+github@pm.me>
2025-11-25 20:35:13 +1000

    Update hyprland.sh

--- hyprland.sh
+++ hyprland.sh
@@ -1,8 +1,9 @@
 sudo dnf copr enable solopasha/hyprland
-sudo dnf install hyprland sddm kitty waybar hyprpolkitagent mako xdg-desktop-portal-hyprland pavucontrol
+sudo dnf install hyprland sddm kitty waybar hyprpolkitagent mako xdg-desktop-portal-hyprland pavucontrol nemo rofi
 sudo systemctl enable sddm.service
 sudo systemctl set-default graphical.target
 systemctl --user enable --now hyprpolkitagent.service
+sudo dnf remove wofi
 
 sudo dnf install dnf-plugins-core
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
