973d01c6cf250536aeae3bbb1d6ecaceab6a5380
Sean <seanwalkerb+github@pm.me>
2025-12-01 11:05:34 +1000

    Create virtualization.sh

---
+++ virtualization.sh
@@ -0,0 +1,3 @@
+sudo dnf install @virtualization
+sudo dnf install gstreamer1-plugins-good gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly
+sudo dnf install gstreamer1-libav
